eb7d71a34699bbc2a2208dc4f5856640a0a2a598
Prashant Sinha <prashnts@live.com>
2015-04-20 22:41:28 +0530

    Revert "Fixed all linter errors."
    This reverts commit 1ed2f6e31445688b6ca6e30df9134286c24a8a7c.

--- assets/js/game-of-life-v3.1.1.js
+++ assets/js/game-of-life-v3.1.1.js
@@ -618,7 +618,7 @@ var GOL = {
     /**
      * Marked for update.
      */
-    canvas_deprecated: {
+    canvas: {
         context : null,
         width : null,
         height : null,
@@ -1386,7 +1386,7 @@ var GOL = {
                 this.drawCell(i, j, false);
             }
         }
-    }
+    },
 };
 
 
